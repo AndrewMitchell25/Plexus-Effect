d984363f67ff9523288fc4438153e2d81b2abbcb
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-03-10 23:17:12 -0800

    Upload Unity Project

---
+++ Design/Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    public int dist;
+    ParticleSystem myParticleSystem;
+    ParticleSystem.Particle[] emittedParticles;
+    public GameObject line;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        emittedParticles = new ParticleSystem.Particle[myParticleSystem.particleCount];
+        myParticleSystem.GetParticles(emittedParticles);
+        
+        for(int i = 0; i < emittedParticles.Length - 1; i++)
+        {
+            for (int j = i + 1; j < emittedParticles.Length - 1; j++)
+            {
+                if (Mathf.Sqrt(Mathf.Pow(emittedParticles[j].position.x - emittedParticles[i].position.x, 2) + Mathf.Pow(emittedParticles[j].position.y - emittedParticles[i].position.y, 2)) <= dist)
+                {
+                    DrawLine(emittedParticles[i].position, emittedParticles[j].position, Color.white);
+                }
+            }
+        }
+        
+        //Debug.DrawLine(emittedParticles[0].position, emittedParticles[1].position, Color.red);
+
+        //myParticleSystem.SetParticles(emittedParticles, emittedParticles.Length);
+    }
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.001f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        lr.SetColors(color, color);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+}
---
+++ Design/Assets/Scripts/PlexusEffect.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlexusEffect : MonoBehaviour
+{
+    public float maxDistance = 1.0f;
+
+    new ParticleSystem particleSystem;
+    ParticleSystem.Particle[] particles;
+
+    ParticleSystem.MainModule particleSystemMainModule;
+
+    public LineRenderer lineRendererTemplate;
+    List<LineRenderer> lineRenderers = new List<LineRenderer>();
+
+    Transform _transform;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystemMainModule = particleSystem.main;
+        _transform = transform;
+    }
+
+    void LateUpdate()
+    {
+        int maxParticles = particleSystemMainModule.maxParticles;
+
+        if(particles == null || particles.Length < maxParticles)
+        {
+            particles = new ParticleSystem.Particle[maxParticles];
+        }
+
+        particleSystem.GetParticles(particles);
+        int particleCount = particleSystem.particleCount;
+
+        float maxDistanceSqr = maxDistance * maxDistance;
+
+        int lrIndex = 0;
+        int lineRendererCount = lineRenderers.Count;
+
+        for(int i = 0; i < particleCount; i++)
+        {
+            Vector3 p1_position = particles[i].position;
+
+            for(int j = i+1; j < particleCount; j++)
+            {
+                Vector3 p2_position = particles[j].position;
+                float distanceSqr = Vector3.Magnitude(p1_position - p2_position);
+
+                if(distanceSqr <= maxDistance)
+                {
+                    LineRenderer lr;
+                    if(lrIndex == lineRendererCount)
+                    {
+                        lr = Instantiate(lineRendererTemplate, _transform, false);
+                        lineRenderers.Add(lr);
+                        lineRendererCount++;
+                    }
+                    lr = lineRenderers[lrIndex];
+
+                    lr.enabled = true;
+
+                    lr.SetPosition(0, p1_position);
+                    lr.SetPosition(1, p2_position);
+                    lr.startColor = particles[i].GetCurrentColor(particleSystem);
+                    lr.endColor = particles[j].GetCurrentColor(particleSystem);
+
+                    lrIndex++;
+                }
+            }
+        }
+
+        for(int i = lrIndex; i < lineRendererCount; i++)
+        {
+            lineRenderers[i].enabled = false;
+        }
+    }
+}
